30eb07a899028c53245facdd170ee7f48162a1fa
cong.nguyen <cong.nguyen@sotatek.com>
2019-06-20 11:34:57 +0700

    update _processingTimeout

--- src/EthCrawler.ts
+++ src/EthCrawler.ts
@@ -1,7 +1,7 @@
 import { BasePlatformCrawler, BlockchainPlatform, ICrawlerOptions } from 'sota-common';
 
 export class EthCrawler extends BasePlatformCrawler {
-  protected _processingTimeout: number = 60000;
+  protected _processingTimeout: number = 120000;
   constructor(options: ICrawlerOptions) {
     super(BlockchainPlatform.Ethereum, options);
   }
