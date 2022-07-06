b2f0a70564a0f2e8e6372549a6556cbd6bb00c59
hoangphuc <hoangphucnb97@gmail.com>
2022-07-06 21:19:44 +0700

    Set web3 provider in case Polygon

--- src/web3.ts
+++ src/web3.ts
@@ -34,7 +34,7 @@ EnvConfigRegistry.onNetworkChanged(network => {
 });
 
 CurrencyRegistry.onCurrencyConfigSet((currency, config) => {
-  if (currency.symbol !== BlockchainPlatform.Ethereum) {
+  if (currency.symbol !== BlockchainPlatform.Ethereum && currency.platform !== BlockchainPlatform.Polygon) {
     return;
   }
 
